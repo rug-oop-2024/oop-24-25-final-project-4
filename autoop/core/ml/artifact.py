b766fc16b3a172993f95e25507d0351503500886
i0999 <rahtorab@gmail.com>
2024-10-22 11:04:52 +0200

    artifact class created

--- autoop/core/ml/artifact.py
+++ autoop/core/ml/artifact.py
@@ -1,5 +1,18 @@
 from pydantic import BaseModel, Field
+
+from abc import ABC
+
 import base64
 
-class Artifact(BaseModel):
-    pass
+class Artifact(ABC, BaseModel):
+    asset_path: str
+    version: str
+    data: bytes
+    metadata: dict
+    type: str
+    tags: list[str]
+
+    @property
+    def id(self) -> str:
+        asset_path_encoded = base64.urlsafe_b64encode(self.asset_path.encode()).decode('utf-8')
+        return f"{asset_path_encoded}:{self.version}"
