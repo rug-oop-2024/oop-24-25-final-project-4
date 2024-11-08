31463c07ff24ea2d1b0cdb4ab257d6fc1fa9d83a
HeleenVanAsselt <heleen@vanasselt.nu>
2024-11-08 18:17:54 +0100

    Uploading datasets Implementation for uploading datasets is added In artifact the saving error for windows was fixed Icons for the pages were changed so they fitted better and new markdwon pages were created for the welcome and instructions pages

--- app/Welcome.py
+++ app/Welcome.py
@@ -2,8 +2,8 @@ from autoop.core.ml.artifact import Artifact
 import streamlit as st
 
 st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
+    page_title="Welcome",
+    page_icon="🏠",
 )
 st.sidebar.success("Select a page above.")
-st.markdown(open("README.md").read())
+st.markdown(open("app/core/welcome.md", encoding="utf-8").read())
---
+++ app/core/instructions.md
@@ -0,0 +1,6 @@
+# Instructions
+
+## Uploading  and using datasets
+To upload datasets, navigate to the datasets page. Here you can upload and name your datasets. 
+
+To use the datasets, navigate to the modelling page and select your desired dataset.
